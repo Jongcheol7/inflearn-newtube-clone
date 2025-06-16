44c1a6a270f492c0a37867d80e86814b7fd98f73
Lee JongCheol <whdcjf817@naver.com>
2025-06-16 23:13:54 +0900

    clerk 인증 설정, layout 설정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-bold">
-      <Image src={"/logo.svg"} height={50} width={50} alt="Logo" />
-      <p className="text-xl font-semibold tracking-tight">NewTube2</p>
-    </div>
-  );
+  return <div className="font-bold">I will load vidoes in the future!</div>;
 }
